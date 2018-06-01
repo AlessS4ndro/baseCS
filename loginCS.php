2989b6fa140a72c0ab2c5710619eb14217ef3099
AlessS4ndro <axelito_b.b@hotmail.com>
2018-06-01 08:41:16 -0500

    trabajo al 10%

--- loginCS.php
+++ loginCS.php
@@ -1,9 +1,15 @@
 <?php
 	//	INICIAMOS LA CONEXION
+	$parametrosPost='cui='.urlencode("20153696").'&dni='.urlencode("76545351");
 	$ch=curl_init();
-	curl_setopt($ch,CURLOPT_URL,'extranet.unsa.edu.pe/admision/or2018f2/escuela444.html');
+	curl_setopt($ch,CURLOPT_URL,'extranet.unsa.edu.pe/sisacad/parciales18/index.php');
 	curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/4.0 (compatible; MSIE 5.01;Windows NT S.0)');
 	curl_setopt($ch,CURLOPT_HTTPHEADER,array("Accept-Language: es-es,en"));
+	
+	curl_setopt($ch,CURLOPT_POST,true);
+	curl_setopt($ch,CURLOPT_POSTFIELDS,$parametrosPost);
+	
+	curl_setopt($ch,CURLOPT_HEADER,false);
 	curl_setopt($ch,CURLOPT_TIMEOUT,10);
 	curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
 	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
@@ -14,7 +20,7 @@
 	curl_close($ch);
 
 	// 	parseamos
-	preg_match_all("(<td class=\"lis-tabl\">LIMA/CHUQUICONDO, GABRIELA NATALIA</td>                <td class=\"lis-tabl\">(.*)</td>)siU",$result,$matches1);
+	preg_match_all("(<td>ARQUITECTURA DE COMPUTADORES</td><td>EVAL. CONTINUA 1</td><td align=center>A</td><td align=center>(.*)</td>)siU",$result,$matches1);
 	
 	$nota = $matches1[1][0];
 	//echo $result;
